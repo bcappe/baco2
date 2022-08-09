c967963fe2adb9d42566fe608649515424dde604
bcappe <bruno.cappellari@outlook.com>
2022-08-09 21:44:16 +0200

    fix IUnitOfWork

--- Core/Interfaces/IUnitOfWork.cs
+++ Core/Interfaces/IUnitOfWork.cs
@@ -1,14 +1,12 @@
+using System;
+using System.Threading.Tasks;
 using Core.Entities;
 
 namespace Core.Interfaces
 {
-    public interface IUnitOfWork: IDisposable
-    {
-         
     public interface IUnitOfWork : IDisposable
     {
         IGenericRepository<TEntity> Repository<TEntity>() where TEntity : BaseEntity;
-        Task<int> Complete(); //cuida quantas coisas mudaram
-    }
+        Task<int> Complete();
     }
 }
