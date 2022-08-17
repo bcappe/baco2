8e186a15ca4a2fd8845e7beeaa186cb47eff3ef3
bcappe <bruno.cappellari@outlook.com>
2022-08-18 00:09:13 +0200

    interim workaround to get workDay without spec

--- Core/Entities/WorkDay.cs
+++ Core/Entities/WorkDay.cs
@@ -1,7 +1,14 @@
+//TODO: remover o constrututor e implementar uma spec
 namespace Core.Entities
 {
     public class WorkDay : WorkDayBase
     {
+        public WorkDay(int employeeId,DateTime date)
+        {
+            EmployeeId=employeeId;
+            Date=date.Date;
+            CheckIn=date;
+        }
         public DateTime Date { get; set; }
         public string? Description { get; set; }    
         public Employee? Employee { get; set; }  
