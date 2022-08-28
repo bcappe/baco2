df34e0e9875eb7afc425889af061f26309432544
BC <bruno.cappellari@outlook.com>
2022-08-28 15:13:52 -0300

    init client

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
+import { AppRoutingModule } from './app-routing.module';
+import { CoreModule } from './core/core.module';
+import { AppComponent } from './app.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    CoreModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ client/src/app/employees/employees.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { EmployeesComponent } from './employees.component';
+
+
+
+
+@NgModule({
+  declarations: [
+    EmployeesComponent,
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class EmployeesModule { }
