2eec5ef16bb6a27e570f8d3ed643559a319be478
Bruno <76883761+bcappe@users.noreply.github.com>
2022-08-31 19:09:09 +0200

    Revert "added pagination to employee"

--- API/Controllers/EmployeeController.cs
+++ API/Controllers/EmployeeController.cs
@@ -1,6 +1,5 @@
 using API.Dtos;
 using API.Erros;
-using API.Helpers;
 using AutoMapper;
 using Core.Entities;
 using Core.Interfaces;
@@ -25,18 +24,16 @@ namespace API.Controllers
             _unitOfWork = unitOfWork;
             _mapper = mapper;
         }
-        [HttpGet]
-        public async Task<ActionResult<Pagination<Employee>>> GetEmployees(
-          [FromQuery] EmployeeSpecParams employeeParams)
+         [HttpGet]
+        public async Task<ActionResult<Employee>> GetEmployees(
+           [FromQuery] EmployeeSpecParams employeeParams)
         {
             var spec = new EmployeesWithWorkScheduleSpecification(employeeParams);
-            var employees = await _unitOfWork.Repository<Employee>().ListAsync(spec);
-            var totalEmployees = await _unitOfWork.Repository<Employee>().CountAsync(spec);
-            return Ok(new Pagination<Employee>(employeeParams.PageIndex,
-                                                       employeeParams.PageSize,
-                                                       totalEmployees,
-                                                       employees));
+            var employee = await _unitOfWork.Repository<Employee>().ListAsync(spec);            
+            return Ok(employee);
         }
+
+
 
         [HttpGet("{id}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
@@ -44,7 +41,7 @@ namespace API.Controllers
         public async Task<ActionResult<Employee>> GetEmployee(int id)
         {
             var spec = new EmployeesWithWorkScheduleSpecification(id);
-            var employee = await _unitOfWork.Repository<Employee>().GetEntityWithSpec(spec);
+            var employee = await _unitOfWork.Repository<Employee>().GetEntityWithSpec(spec);    
             if (employee == null) return NotFound(new ApiResponse(404));
             return employee;
         }
@@ -52,11 +49,11 @@ namespace API.Controllers
         public async Task<ActionResult> DeleteEmployee(int id)
         {
             var employee = await _unitOfWork.Repository<Employee>().GetByIdAsync(id);
-
+            
             _unitOfWork.Repository<Employee>().Delete(employee);
 
             var result = await _unitOfWork.Complete();
-
+            
             if (result <= 0) return BadRequest(new ApiResponse(400, "Problem deleting workDay"));
 
             return Ok();
@@ -64,9 +61,9 @@ namespace API.Controllers
         [HttpPut("{id}")]
         public async Task<ActionResult<Employee>> UpdateEmployee(int id, EmployeeDto employeeDto)
         {
-            var employee = await _unitOfWork.Repository<Employee>().GetByIdAsync(id);
-            _mapper.Map(employeeDto, employee);
-
+            var employee = await _unitOfWork.Repository<Employee>().GetByIdAsync(id);     
+            _mapper.Map(employeeDto, employee);  
+                        
             _unitOfWork.Repository<Employee>().Update(employee);
 
             var result = await _unitOfWork.Complete();
@@ -78,9 +75,9 @@ namespace API.Controllers
         [HttpPost]
         public async Task<ActionResult<Employee>> AddWorkSchedule(EmployeeDto createEmployee)
         {
-            var employee = _mapper.Map<EmployeeDto, Employee>(createEmployee);
+            var employee = _mapper.Map<EmployeeDto,Employee>(createEmployee);
             _unitOfWork.Repository<Employee>().Add(employee);
-
+            
             var result = await _unitOfWork.Complete();
 
             if (result <= 0) return BadRequest(new ApiResponse(400, "Problem creating entry"));
--- Core/Specifications/ParametersSpecificatios/EmployeeSpecParams.cs
+++ Core/Specifications/ParametersSpecificatios/EmployeeSpecParams.cs
@@ -4,14 +4,15 @@ namespace Core.Specifications
 {
     public class EmployeeSpecParams
     {
-        private const int MaxPageSize = 50;
-        public int PageIndex { get; set; } = 1;
-        private int _pageSize = 6;
-        public int PageSize
-        {
-            get => _pageSize;
-            set => _pageSize = (value > MaxPageSize) ? MaxPageSize : value;
-        }
+        //TODO: falta implementar pagination
+        // private const int MaxPageSize = 50;
+        // public int PageIndex { get; set; } = 1;
+        // private int _pageSize = 6;
+        // public int PageSize
+        // {
+        //     get => _pageSize;
+        //     set => _pageSize = (value > MaxPageSize) ? MaxPageSize : value;
+        // }
         public EmployeeSpecParams()
         {
         }
@@ -20,7 +21,7 @@ namespace Core.Specifications
         public string? Rfid { get; set; }
 
         public string? Sort { get; set; }
-
+        
         private string? _search;
         public string? Search
         {
